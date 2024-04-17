b75a3a533c5b2aeef676b9bbb37e6df3bfd86d6d
TeoBarani <127655586+TeoBarani@users.noreply.github.com>
2024-04-16 22:14:43 -0300

    Add files via upload

--- ejercicios_practica/marvel/e_commerce/api/urls.py
+++ ejercicios_practica/marvel/e_commerce/api/urls.py
@@ -4,9 +4,9 @@ from e_commerce.api.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('comic-list/', comic_list_api_view),
-    path('comic-retrieve/', comic_retrieve_api_view),
-    path('comic-create/', comic_create_api_view),
-    path('comic-list-filter/', comic_list_filtered_api_view),
+    path('comic_list_api_view/', comic_list_api_view),
+    path('comic_retrieve_api_view/', comic_retrieve_api_view),
+    path('comic_create_api_view/', comic_create_api_view),
+    path('comic_list_filtered_api_view/', comic_list_filtered_api_view),
     path('comic-title-filter/', comic_retrieve_filtered_api_view),
 ]
